9ae2c1bb818df70319cb0b284e6f1b52657cfe12
Patrascu Bogdan <patrascu.bogdan92@gmail.com>
2020-04-06 16:55:48 +0300

    finished the SingleMovieViewModel

--- app/src/main/java/com/example/movies/single_movie_details/SingleMovieViewModel.kt
+++ app/src/main/java/com/example/movies/single_movie_details/SingleMovieViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.movies.single_movie_details
 
-class SingleMovieViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.movies.data.repository.NetworkState
+import com.example.movies.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class SingleMovieViewModel (private val movieRepository: MovieDetailsRepository, movieId: Int) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    val movieDetails : LiveData<MovieDetails> = movieRepository.fetchSingleMovieDetails(compositeDisposable, movieId)
+    val networkState : LiveData<NetworkState> = movieRepository.getMovieDetailsNetworkState()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 }
