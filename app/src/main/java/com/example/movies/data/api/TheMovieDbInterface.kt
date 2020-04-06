f885d4a75abc3214e9419c2b43ffb6fc743aebd2
Patrascu Bogdan <patrascu.bogdan92@gmail.com>
2020-04-06 19:27:51 +0300

    Added MovieResponse VO , extra method in interface to get popular movies and movie_list_item layout

--- app/src/main/java/com/example/movies/data/api/TheMovieDbInterface.kt
+++ app/src/main/java/com/example/movies/data/api/TheMovieDbInterface.kt
@@ -1,11 +1,16 @@
 package com.example.movies.data.api
 
 import com.example.movies.data.vo.MovieDetails
+import com.example.movies.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDbInterface {
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
