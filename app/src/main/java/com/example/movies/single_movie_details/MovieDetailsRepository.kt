3c680abf438cc79837f23484909bfcb7de3d1c9d
Patrascu Bogdan <patrascu.bogdan92@gmail.com>
2020-04-06 16:48:01 +0300

    Added MovieDetailsRepository and SingleMovieViewModel

---
+++ app/src/main/java/com/example/movies/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.movies.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.movies.data.api.TheMovieDbInterface
+import com.example.movies.data.repository.MovieDetailsNetworkDataSource
+import com.example.movies.data.repository.NetworkState
+import com.example.movies.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository(private val apiService: TheMovieDbInterface) {
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails(compositeDisposable: CompositeDisposable, movieId: Int) : LiveData<MovieDetails> {
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService, compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieDetailsResponse
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState> {
+        return movieDetailsNetworkDataSource.networkState
+    }
+    // might seem like an extra step, but this would come in handy if we would want to cache de data in local storage
+}
---
+++ app/src/main/java/com/example/movies/single_movie_details/SingleMovieViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.movies.single_movie_details
+
+class SingleMovieViewModel {
+}
